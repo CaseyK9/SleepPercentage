90e74cdd73dbca224a2312285ac590f497435219
CaseyK9 <40074095+HyenaLtd@users.noreply.github.com>
2020-11-02 12:47:35 -0500

    Create EventConfig.java

---
+++ src/com/sammurphy/sleepPercentage/eventManagement/EventConfig.java
@@ -0,0 +1,7 @@
+package com.sammurphy.sleepPercentage.eventManagement;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public interface EventConfig {
+    FileConfiguration getConfig();
+}
